8e35eb0aa413208e66ce714eef218804c691b624
Yeriimii <87357932+Yeriimii@users.noreply.github.com>
2023-04-05 02:49:42 +0900

    chap_02 Operator
    산술 연산자

--- src/chap_01/_Quiz_01.java
+++ src/chap_01/_Quiz_01.java
@@ -10,7 +10,7 @@ public class _Quiz_01 {
         String busNo = "상암08";
         int min = 3;
         double distance = 1.2;
-            
+
         System.out.println(busNo + "번 버스");
         System.out.println("약 " + min + "분 후 도착");
         System.out.println("남은 거리 " + distance + "km");
